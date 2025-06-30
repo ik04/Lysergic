4993145cc61355f183c98fb0541ed6c5761e6bf7
ik04 <ishaankhurana123@gmail.com>
2025-06-30 22:13:43 +0530

    feat: fix typo in cors

--- server/rest/core/config.py
+++ server/rest/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     
     DATABASE_URL: Optional[str] = None
     
-    BACKEND_CORS_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:8000","https://lysergic.vercel.app/"]
+    BACKEND_CORS_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:8000","https://lysergic.vercel.app"]
     
     DEBUG: bool = False
     
