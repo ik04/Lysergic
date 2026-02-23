7ed80894e5f1c3bf112e4bb49d91e1e123220f2e
ik04 <ishaankhurana123@gmail.com>
2026-02-23 14:45:44 +0530

    feat: add cf web analytics

--- client/web/app/root.tsx
+++ client/web/app/root.tsx
@@ -34,6 +34,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        {/* Cloudflare Web Analytics */}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "7c98740fce3f471c972621c7e3654b7a"}'
+        ></script>
+        {/* End Cloudflare Web Analytics */}
       </head>
       <body>
         {children}
