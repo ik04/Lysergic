73a67f33b5197849321f735b577d88d5e0de9d70
ik04 <ishaankhurana123@gmail.com>
2026-02-23 16:49:14 +0530

    feat: add hotjar

--- client/web/app/root.tsx
+++ client/web/app/root.tsx
@@ -34,13 +34,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        {/* Cloudflare Web Analytics */}
         <script
-          defer
-          src="https://static.cloudflareinsights.com/beacon.min.js"
-          data-cf-beacon='{"token": "7c98740fce3f471c972621c7e3654b7a"}'
-        ></script>
-        {/* End Cloudflare Web Analytics */}
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:YOUR_ID,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `,
+          }}
+        />
       </head>
       <body>
         {children}
